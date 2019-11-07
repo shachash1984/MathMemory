4a699e77c56c23a4d68f6c4150fa5509c567ad9a
shachash1984 <shachash1984@gmail.com>
2019-11-07 10:20:17 +0200

    initial commit

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum CardType { Question, Answer}
+public enum Action { Add = 0, Subtract = 1 }//, Multiply = 2, Divide = 3}
+
+public class Card : MonoBehaviour {
+
+    #region Properties
+    public Texture backSprite;
+    //public Texture frontSprite;
+    [SerializeField] private Text cardText;
+    private int result;
+    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] private GameObject _v;
+    [SerializeField] private Button _button;
+    [SerializeField] private RawImage _cardImage;
+    [SerializeField] private Texture _backTexture;
+    [SerializeField] private Texture _frontTexture;
+    private CardType cardType;
+    [HideInInspector] public RectTransform rTransform;
+    public bool isShowing;
+    public bool foundMatch;
+    public bool openedOnce;
+    
+    
+    
+    #endregion      
+
+    #region MonoBehaviour Callbacks
+    void Awake()
+    {
+        isShowing = false;
+        Flip(false);
+        foundMatch = false;
+        openedOnce = false;
+        
+        rTransform = GetComponent<RectTransform>();             
+    }
+
+    void Start()
+    {
+        _button.onClick.AddListener(
+            () =>
+            {
+                if (!GameManager.S.isAnimating && !GameManager.S.areTwoCardsOpen)
+                    StartCoroutine(GameManager.S.Flip(this));
+            });
+    }
+    #endregion
+
+    #region Methods
+    public override bool Equals(object other)
+    {
+        Card a = other as Card;
+        return this.result == a.GetResult();       
+    }       
+
+    public CardType GetCardType()
+    {
+        return cardType;
+    }
+
+    public void SetCardType(CardType ct)
+    {
+        cardType = ct;
+    }
+
+    public int GetResult()
+    {
+        return result;
+    }
+
+    public void SetResult(int r)
+    {
+        result = r;
+    }
+
+    public string GetCardText()
+    {
+        return cardText.text;
+    }
+
+    public void SetCardText(string s)
+    {
+        cardText.text = s;
+    }
+
+    public void CreateQuestion(Action act)
+    {
+        int a = 0;
+        int b = 0;
+        switch (act)
+        {
+            case Action.Add:  
+                a = UnityEngine.Random.Range(0, result);
+                b = result - a;
+                SetCardText(string.Format("{0} + {1}", a, b));
+                break;
+            case Action.Subtract:
+                a = UnityEngine.Random.Range(result, result*2);
+                b = a - result;
+                SetCardText(string.Format("{0} - {1}", a, b));
+                break;
+            /*case Action.Multiply:
+                if (result == 0 || result == 1)
+                    goto Add;
+                List<int> resultMultipliers = new List<int>();                
+                for (int i = 2; i < result; i++)
+                {                    
+                    if(i == 2 && result % 2 == 0)
+                    {
+                        resultMultipliers.Add(i);
+                        Debug.Log("multiplier i: " + i + "result: " + result);
+                    }
+                    else if (result % i == 0)
+                    {
+                        resultMultipliers.Add(i);
+                        Debug.Log("multiplier i: " + i + "result: " + result);
+                    }
+                        
+                }
+                if (resultMultipliers.Count > 0)
+                {
+                    a = UnityEngine.Random.Range(1, resultMultipliers[resultMultipliers.Count - 1]);
+                    b = result / a;
+                    SetCardText(string.Format("{0} X {1}", a, b));
+                }
+                
+                               
+                              
+                break;
+            case Action.Divide:
+                b = UnityEngine.Random.Range(1, 15);
+                a = result * b;                
+                SetCardText(string.Format("{0} / {1}", a, b));
+                break;
+            default:
+                break;*/
+        }
+    }
+
+    public bool GetIsShowing()
+    {
+        return isShowing;
+    }
+
+    public void SetIsShowing(bool show) 
+    {
+        isShowing = show;
+    }
+
+    public void Flip(bool show) 
+    {
+        if (show)
+        {
+            if (!openedOnce)
+                openedOnce = true;
+            _cardImage.texture = _frontTexture;
+            cardText.gameObject.SetActive(true);
+            isShowing = true;            
+        }
+        else
+        {
+            _cardImage.texture = _backTexture;
+            cardText.gameObject.SetActive(false);
+            isShowing = false;
+        }
+    }
+    #endregion
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,380 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour {
+
+    #region Properties
+    public bool gameOver;
+    public bool gamePaused;
+    public int pairsFound;
+    public float timeLeft;
+    public int score;
+    public GameObject endGamePanel;
+    [SerializeField] private Card CardPrefab;
+    [SerializeField] private List<Texture> cardFaces;
+    /*[SerializeField]*/
+    private List<Vector3> cardPositions = new List<Vector3>();
+    [SerializeField] private GameObject panel;
+    [SerializeField] private Texture BGSprite;
+    [SerializeField] private Texture startSprite;
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private List<Card> cards = new List<Card>();
+    private int cardAmt = 12;
+    [SerializeField] private Text timeText;
+    [SerializeField] private Text scoreText;
+    private int _score;
+    public AudioSource audioSource;
+    public AudioClip flipSound;
+    public AudioClip successSound;
+    public AudioClip winMusic;
+    public AudioClip failSound;
+    [HideInInspector] public bool isAnimating = false;
+    [HideInInspector] public bool areTwoCardsOpen = false;
+    #endregion
+
+    #region Constructor (Singleton)
+    static public GameManager S;
+    
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    void Awake()
+    {
+        if (S != null)
+            Destroy(this.gameObject);
+        else
+            S = this;
+        LoadStartScreen();  
+    }    
+
+    void Update()
+    {        
+        if (timeLeft <= 0f && !gameOver && !gamePaused)
+            timeLeft = 0;
+        else if (!gameOver && !gamePaused)
+            timeLeft -= Time.deltaTime;
+
+        try
+        {
+            if (timeLeft < 100)
+                timeText.text = "Time Left: " + timeLeft.ToString().Remove(4);
+            else
+                timeText.text = "Time Left: " + timeLeft.ToString().Remove(3);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            timeText.text = "Time Left: " + timeLeft.ToString();
+        }        
+
+        if (!gameOver &&(pairsFound >= 6 || timeLeft == 0f))
+            StartCoroutine(EndGame());
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Quit();
+
+    }
+    #endregion
+
+    #region Methods
+    void LoadStartScreen()
+    {
+        gameOver = true;        
+        endGamePanel.SetActive(true);
+        //endGamePanel.GetComponentInChildren<Text>().text = "";
+        
+    }
+
+    private int GetScore()
+    {
+        return _score;
+    }
+
+    private void AddScore(int scoreToAdd)
+    {
+        _score += scoreToAdd;
+        if (_score < 0)
+            _score = 0;
+    }
+
+    private void SetScoreText()
+    {
+        scoreText.text = string.Format("Score: {0}", GetScore());
+    }
+
+    public void Init()
+    {
+        Card[] oldCards = FindObjectsOfType<Card>();
+        for (int i = 0; i < oldCards.Length; i++)
+        {
+            Destroy(oldCards[i].gameObject);
+        }
+        _score = 0;
+        SetScoreText();
+        StartCoroutine(SetCards());
+    }
+
+    public IEnumerator SetCards()
+    {
+        Debug.Log("Init");
+        ResetTimer();
+        foreach (Card c in cards)
+        {            
+            Destroy(c.gameObject);
+        }
+        cards.Clear();
+        cardPositions.Clear();
+        gameOver = false;
+        pausePanel.SetActive(false);
+        
+        endGamePanel.SetActive(false);
+        for (int i = 0; i < cardAmt; i++) //restting position list
+        {
+            if (i < 3)
+                cardPositions.Add(new Vector3((i * 130) - 160, 160));
+            else if (i < 6)            
+                cardPositions.Add(new Vector3(((i % 3) * 130) - 160, 60f));         
+            else if (i < 9)            
+                cardPositions.Add(new Vector3(((i % 3) * 130) - 160, -40f));            
+            else
+                cardPositions.Add(new Vector3(((i % 3) * 130) - 160, -140f));
+
+        }
+        //Debug.Log("cardPositions.Count: " + cardPositions.Count);
+        //Sequence seq = DOTween.Sequence();
+        for (int i = 0; i < cardAmt; i++)
+        {
+            yield return new WaitForSeconds(0.1f);
+            Card tempCard = Instantiate(CardPrefab, new Vector3(0,-340), Quaternion.identity, panel.transform) as Card;
+            tempCard.GetComponent<RawImage>().texture = tempCard.backSprite;
+            if (i % 2 == 0)
+            {
+                tempCard.SetCardType(CardType.Answer);
+                tempCard.SetResult(UnityEngine.Random.Range(0, 100));
+                tempCard.SetCardText(tempCard.GetResult().ToString());
+            }
+            else
+            {
+                tempCard.SetCardType(CardType.Question);
+                tempCard.SetResult(cards[i - 1].GetResult());
+                int rand = UnityEngine.Random.Range(0, 2);
+                Action a = (Action)rand;
+               // Debug.Log(a.ToString());
+                tempCard.CreateQuestion(a);
+            }
+            //int index = UnityEngine.Random.Range(0, cardPositions.Count);
+            //Debug.Log("index: " + index);
+            /*seq.Append(*/
+            tempCard.rTransform.DOLocalMove(cardPositions[i], 0.75f);                       
+            //cardPositions.RemoveAt(index);
+            cards.Add(tempCard);     
+            if(cards.Count == cardAmt)
+            {
+                StartCoroutine(ShuffleCards());
+            }
+        }
+        
+        //seq.Play<Sequence>();
+
+            
+    }
+
+    private void ResetTimer()
+    {
+        timeLeft = 120f;        
+    }
+
+    IEnumerator EndGame()
+    {        
+        score = pairsFound;        
+        gameOver = true;
+        pairsFound = 0;        
+        yield return new WaitForSeconds(1f);
+        //endGamePanel.GetComponent<RawImage>().texture = BGSprite;
+        endGamePanel.SetActive(true);
+        if (timeLeft > 0)
+        {
+            endGamePanel.GetComponentInChildren<Text>().text = string.Format("Good Job! \n You scored {0} points!", GetScore());
+            audioSource.clip = winMusic;
+            audioSource.Play();
+        }
+
+        else
+        {
+            endGamePanel.GetComponentInChildren<Text>().text = string.Format("Time's Up! \nYou scored {0} points", GetScore());
+            audioSource.clip = failSound;
+            audioSource.Play();
+        }
+            
+        StopCoroutine(EndGame());
+    }
+
+    public bool CompareCards(Card c, Card d)
+    {        
+        if (c.isShowing && d.isShowing && (!c.foundMatch || !d.foundMatch))
+        {
+            c.foundMatch = c.Equals(d);
+            d.foundMatch = c.foundMatch;
+            if(c.foundMatch)
+            {
+                audioSource.clip = successSound;
+                audioSource.Play();
+            }
+            return c.foundMatch;
+        }                
+        else
+            return false;        
+    }
+
+    public IEnumerator Flip(Card c)
+    {
+        
+        if (!gameOver)
+        {
+            isAnimating = true;
+            List<Card> openCards = new List<Card>();
+            int openCardsCounter = 0;
+            c.SetIsShowing(true);
+            for (int i = 0; i < cards.Count; i++)
+            {
+                if (cards[i].isShowing && !cards[i].foundMatch)
+                {
+                    openCards.Add(cards[i]);
+                    openCardsCounter++;
+                    //Debug.Log("openCards.Count: " + openCards.Count);
+                }
+                    
+            }
+            if (openCardsCounter <= 2)
+            {
+                if (openCardsCounter == 2)
+                    areTwoCardsOpen = true;
+                c.GetComponent<Animator>().Play("Flip");
+                audioSource.clip = flipSound;
+                audioSource.Play();
+                yield return new WaitForSeconds(0.33f);
+                c.Flip(true);                     
+                CheckOpenCards(openCards);
+            }
+            yield return new WaitUntil(() => c.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Idle"));
+            isAnimating = false;
+        }        
+    }
+
+    public void CheckOpenCards(List<Card> openCards)
+    {
+        if (!gameOver)
+        {
+            if(openCards.Count > 1)
+            {
+                if (CompareCards(openCards[0], openCards[1]))
+                {
+                    isAnimating = true;
+                    foreach (Card t in openCards)
+                    {
+                        t.foundMatch = true;
+                        t.GetComponent<Button>().enabled = false;
+                        t.GetComponent<Animator>().Play("Correct");
+                        AddScore(100);
+                        SetScoreText();
+                        cards.Remove(t);                        
+                    }
+                    pairsFound++;
+                    isAnimating = false;
+                    areTwoCardsOpen = false;
+                    return;
+                }
+                else
+                    StartCoroutine(HideUnMatchingCards(openCards.ToArray()));
+            }
+            
+            /*List<Card> pair = new List<Card>();
+            for (int i = 0; i < cards.Count; i++)
+            {
+                if (!cards[i].foundMatch && cards[i].isShowing)
+                    pair.Add(cards[i]);
+
+                if (pair.Count == 2)
+                {
+                    if (CompareCards(pair[0], pair[1]))
+                    {
+                        foreach (Card t in pair)
+                        {
+                            t.foundMatch = true;
+                            t.GetComponent<Button>().enabled = false;
+                            t.GetComponent<Animator>().Play("Correct");
+                        }
+                        pairsFound++;                        
+                        return;
+                    }
+                    else
+                        StartCoroutine(HideUnMatchingCards(pair.ToArray()));
+                }
+            }*/
+        }        
+    }
+
+    public IEnumerator HideUnMatchingCards(params Card[] pair)
+    {
+        isAnimating = true;
+        yield return new WaitForSeconds(1f);
+        int counter = 0;
+        for (int i = 0; i < pair.Length; i++)
+        {
+            pair[i].GetComponent<Animator>().Play("Flip");
+            yield return new WaitForSeconds(0.33f);
+            pair[i].SetIsShowing(false);
+            pair[i].Flip(false);
+            if (pair[i].openedOnce)
+                counter++;
+        }
+        if (counter > 0)
+            AddScore(-50 * counter);
+        SetScoreText();
+        yield return new WaitForSeconds(0.33f);
+        isAnimating = false;
+        areTwoCardsOpen = false;
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+        gamePaused = true;
+    }
+
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+        gamePaused = false;
+    }
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public IEnumerator ShuffleCards()
+    {
+        yield return new WaitForSeconds(1f);
+        isAnimating = true;
+        DOTween.CompleteAll();        
+        List<Vector3> positions = new List<Vector3>(cardPositions.ToArray());
+        List<Card> tempCards = new List<Card>(cards.ToArray());
+        int rand = 0;
+        foreach (Card c in tempCards)
+        {
+            rand = UnityEngine.Random.Range(0, positions.Count);
+            c.rTransform.localPosition = positions[rand];
+            positions.Remove(positions[rand]);
+        }
+        isAnimating = false;
+    }
+    #endregion
+}
